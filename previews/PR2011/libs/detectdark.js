48eb1edecf3e4f2dd5ba75074cf93d17be72ee94
IanButterworth <IanButterworth@users.noreply.github.com>
2024-01-06 02:36:41 +0000

    Deploying to gh-preview from @ JuliaLang/www.julialang.org@f847879bdcb81628ef12c6438e7f470279f17c3e 🚀

--- previews/PR2011/libs/detectdark.js
+++ previews/PR2011/libs/detectdark.js
@@ -1,18 +1,24 @@
 var targetNode = document.documentElement;
 var config = { attributes: true, childList: false, subtree: false };
 var darkreader_field = 'data-darkreader-scheme';
-
+function changeFilePath(originalPath, newFileName) {
+    const pathArray = originalPath.split('/');
+    const fileNameIndex = pathArray.length - 1;
+    pathArray[fileNameIndex] = newFileName;
+    const modifiedPath = pathArray.join('/');
+    return modifiedPath;
+  }
 function check_scheme() {
     if (targetNode.getAttribute(darkreader_field) == 'dark') {
         console.log("darkreader dark mode detected");
-        var julialogo = "/assets/infra/logo-dark.svg";
+        var julialogo = "logo-dark.svg";
     } else {
         console.log("darkreader non-dark mode detected");
-        var julialogo = "/assets/infra/logo.svg";
+        var julialogo = "logo.svg";
     }
     var imgElements = document.getElementsByClassName('julialogo');
     for (var i = 0; i < imgElements.length; i++) {
-        imgElements[i].src = julialogo;
+        imgElements[i].src = changeFilePath(imgElements[i].src, julialogo);
     }
 }
 
